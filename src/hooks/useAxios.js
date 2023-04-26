6f0f61ae9991ebd1b57d12bdf21433f3a4e79f47
Akshay Kulkarni <Akshay.Kulkarni@publicissapient.com>
2023-04-26 11:23:05 +0530

    axios folder structure

---
+++ src/hooks/useAxios.js
@@ -0,0 +1,10 @@
+export const useAxios = () => {};
+
+// useEffect(() => {
+//   const fn = async () => {
+//     const data = await pokeAPI.get('pokemon/ditto');
+//     console.log(data.data);
+//   };
+
+//   fn();
+// }, []);
