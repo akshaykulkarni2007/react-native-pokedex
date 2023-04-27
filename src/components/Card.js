f2d617696b612e71f9b4fae554b7a1934c79451d
Akshay Kulkarni <Akshay.Kulkarni@publicissapient.com>
2023-04-27 11:28:10 +0530

    start load more

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,6 @@ import {View, Text, Image} from 'react-native';
 import {cardStyles} from './styles';
 
 export const Card = ({image, title, description, bgcolor}) => {
-  console.log(image);
   return (
     <View style={[cardStyles.container, bgcolor]}>
       <Image source={{uri: image}} style={cardStyles.image} />
--- src/screens/PokemonListingScreen/index.js
+++ src/screens/PokemonListingScreen/index.js
@@ -1,6 +1,6 @@
-import axios from 'axios';
 import {useState, useEffect} from 'react';
 import {View, Text, FlatList} from 'react-native';
+import axios from 'axios';
 
 import {Spinner, Card} from '../../components';
 
@@ -9,30 +9,55 @@ import {API_BASE_URL} from '../../constants';
 import styles from './styles';
 
 export const PokemonListingScreen = () => {
+  const [loading, setLoading] = useState(false);
   const [pokemons, setPokemons] = useState([]);
+  const [hasScrolled, setHasScrolled] = useState(false);
+  const [nextURL, setNextURL] = useState('');
 
   useEffect(() => {
     fetchPokemons();
   }, []);
 
   const fetchPokemons = async () => {
-    const {data: pokemonList} = await axios.get(
-      `${API_BASE_URL}pokemon?limit=2`,
-    );
+    setLoading(true);
 
-    pokemonList.results.forEach(async item => {
-      const {data: details} = await axios.get(item.url);
+    try {
+      const {data: pokemonList} = await axios.get(
+        `${API_BASE_URL}pokemon?limit=2`,
+      );
 
-      setPokemons(prev => [
-        ...prev,
-        {
-          name: details.name,
-          imageUrl: details.sprites.front_default,
-          types: details.types.map(type => type.type.name),
-        },
-      ]);
-    });
+      console.log(pokemonList);
+
+      pokemonList.results.forEach(async item => {
+        const {data: details} = await axios.get(item.url);
+
+        setPokemons(prev => [
+          ...prev,
+          {
+            name: details.name,
+            imageUrl: details.sprites.front_default,
+            types: details.types.map(type => type.type.name),
+          },
+        ]);
+      });
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
   };
+
+  const handleLoadMore = async () => {
+    if (!hasScrolled) {
+      return null;
+    }
+    console.log('scrolled');
+    //here load data from your backend
+  };
+
+  if (loading) {
+    return <Spinner />;
+  }
 
   return (
     <View style={styles.container}>
@@ -49,11 +74,14 @@ export const PokemonListingScreen = () => {
             title={item.name}
             image={item.imageUrl}
             description={`${index + 1}`.padStart(3, '0')}
-            // bgcolor={'red'}
+            // bgcolor={{backgroundColor: 'red'}}
           />
         )}
         numColumns={2}
         columnWrapperStyle={styles.cardColumnWrapperStyle}
+        onEndReached={handleLoadMore}
+        onScroll={() => setHasScrolled(true)}
+        onEndThreshold={50}
       />
     </View>
   );
